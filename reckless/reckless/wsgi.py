53a6cddb877a6e9c1740d14e08ecc83c71128ce0
Nate <natewillis1@gmail.com>
2024-10-29 03:16:10 +0000

    fixed wsgi

--- reckless/reckless/wsgi.py
+++ reckless/reckless/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reckless.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reckless.settings.production')
 
 application = get_wsgi_application()
